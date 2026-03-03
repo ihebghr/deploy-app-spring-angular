079d8b59b6488c39847a0c71f5afc50e53c14534
ihebghr <gharbiiheb002@gmail.com>
2026-03-03 13:02:31 +0100

    finalisation

--- springboot/app/src/main/java/com/oauth/demo/controllers/UserController.java
+++ springboot/app/src/main/java/com/oauth/demo/controllers/UserController.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/user")
-@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     @Autowired
     UserService userService;
